c72d2f3c5421ddc458cf1d0a55bcb7f276c5b642
Shibu-Patil <patilshibu@gmail.com>
2025-12-25 11:04:58 +0530

    fixe mongo

--- config/db.js
+++ config/db.js
@@ -3,18 +3,15 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     if (!process.env.MONGO_URI) {
-      throw new Error("MONGO_URI is not defined in .env");
+      throw new Error("MONGO_URI is not defined");
     }
 
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const conn = await mongoose.connect(process.env.MONGO_URI);
 
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
-    console.error(`Error connecting to MongoDB: ${error.message}`);
-    process.exit(1); // Exit process if DB connection fails
+    console.error("Error connecting to MongoDB:", error.message);
+    process.exit(1);
   }
 };
 
